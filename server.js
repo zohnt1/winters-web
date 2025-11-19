a86fdaa3865fbcdc833a97f6b539be0eaf8d7d5f
zohnt1 <zohntdiscordbusiness@gmail.com>
2025-11-19 17:00:42 -0500

    Add files via upload
    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+// view engine + views folder
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// static files folder (optional)
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// home route -> renders views/index.ejs
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+
+// start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
